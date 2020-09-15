a5d1e9f6d7700cdd9ff19b2f40ed4503a8e75e7b
harrison1991 <kye0038883@gmail.com>
2020-09-15 18:30:08 +0800

    update es6 uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 'use strict';
  
-//npm install gulp-babel @babel/core @babel/preset-env
+//es6: npm install gulp-babel @babel/core @babel/preset-env
 
 const {src, dest, task, series, parallel} = require('gulp'),
+    argv = require('yargs').argv,
     sass = require('gulp-dart-sass'),
     watcher = require('gulp-watch'),
     plumber = require('gulp-plumber'),
@@ -15,58 +16,70 @@ const {src, dest, task, series, parallel} = require('gulp'),
     gulpRename = require('gulp-rename'),
     notify = require('gulp-notify'),
     gulpClean = require('gulp-clean'),
+    gulpIf = require('gulp-if'),
+    gulpUglify = require('gulp-uglify-es').default,
+    /**
+     * config
+     */
     sourcePath = {
-    sass: './workshop/**/css/*.scss',
-    js: './workshop/**/js/*.js',
-    distPagesHtml: './dist/02_pages/**/!(_)*.html',
-    workshopHtml: './workshop/**/!(_)*.html',
-    moveToDist: './workshop/**/*',
-    dist: './dist/',
-    public: './public/',
-    layout: './workshop/01_system/layout/',
-    export: ['./dist/*', './public/*']
-},
-convertSass = () => {
-    return src(sourcePath.sass)
-        .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(dest(sourcePath.dist));
-},
-convertES6 = () => {
-    return src(sourcePath.js)
-        .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
-        .pipe(babel({
-            presets: ['@babel/env'],
-            minified: true
-        }))
-        .pipe(dest(sourcePath.dist));
-},
-cleanExportFile = () => {
-    return src(sourcePath.export, {read: false})
-        .pipe(gulpClean());
-},
-convertHtmlInclude = () => {
-    return src(sourcePath.workshopHtml)
-        .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
-        .pipe(htmlInclude({
-            data: {
-                "foo" : "bar"
-            }
-        }))
-        .pipe(htmlLayout({annotations:true, verbose:false, root: sourcePath.layout}))
-        .pipe(htmlMin({ collapseWhitespace: true }))
-        .pipe(dest(sourcePath.dist));
-},
-convertHtmlInline = () => {
-    return src(sourcePath.distPagesHtml)
-        .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
-        .pipe(inlineSource())
-        .pipe(gulpRename((path) => {
-            path.dirname = ''
-        }))
-        .pipe(dest(sourcePath.public));
-};
-
+        sass: './workshop/**/css/*.scss',
+        js: './workshop/**/js/*.js',
+        distPagesHtml: './dist/02_pages/**/!(_)*.html',
+        workshopHtml: './workshop/**/!(_)*.html',
+        moveToDist: './workshop/**/*',
+        dist: './dist/',
+        public: './public/',
+        layout: './workshop/01_system/layout/',
+        export: ['./dist/*', './public/*']
+    },
+    /**
+     * task
+     */
+    convertSass = () => {
+        return src(sourcePath.sass)
+            .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+            .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+            .pipe(dest(sourcePath.dist));
+    },
+    convertES6 = () => {
+        return src(sourcePath.js)
+            .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+            .pipe(gulpIf(argv.production, gulpUglify({
+                compress: true,
+                keep_classnames: false,
+                keep_fanems: false
+            })))
+            .pipe(gulpIf(!argv.production, gulpUglify()))
+            .pipe(dest(sourcePath.dist));
+    },
+    cleanExportFile = () => {
+        return src(sourcePath.export, {read: false})
+            .pipe(gulpClean());
+    },
+    convertHtmlInclude = () => {
+        return src(sourcePath.workshopHtml)
+            .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+            .pipe(htmlInclude({
+                data: {
+                    "foo" : "bar"
+                }
+            }))
+            .pipe(htmlLayout({annotations:true, verbose:false, root: sourcePath.layout}))
+            .pipe(gulpIf(argv.production, htmlMin({ collapseWhitespace: true })))
+            .pipe(dest(sourcePath.dist));
+    },
+    convertHtmlInline = () => {
+        return src(sourcePath.distPagesHtml)
+            .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+            .pipe(inlineSource())
+            .pipe(gulpRename((path) => {
+                path.dirname = ''
+            }))
+            .pipe(dest(sourcePath.public));
+    };
+/**
+ * watcher
+ */
 task('scssWatch', (cb) => {
     watcher(sourcePath.sass, series(convertSass, convertHtmlInclude, convertHtmlInline));
     cb();
@@ -87,5 +100,8 @@ task('htmlInlineWatch', (cb) => {
     cb()
 });
 
+/**
+ * task group
+ */
 task('watcher', series( parallel('scssWatch', 'es6Watch'), 'htmlIncludeWatch', 'htmlInlineWatch'));
 task('build',  series( cleanExportFile, parallel(convertSass, convertES6), convertHtmlInclude, convertHtmlInline, 'watcher'));
